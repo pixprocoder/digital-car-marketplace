4011cad2f914ce82f2d67e8d956b458d43253d62
samsul kobir <makkobir79@gmail.com>
2024-01-14 00:17:25 +0200

    tyignore added to nextconfig file

--- src/components/pages/home/features/FeatureServices.tsx
+++ src/components/pages/home/features/FeatureServices.tsx
@@ -20,7 +20,7 @@ const FeatureServices = () => {
           <Button type="submit">Search</Button>
         </div>
       </div>
-      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
         {data?.map((service: any) => (
           <FeatureServiceCard key={service.serviceId} service={service} />
         ))}
