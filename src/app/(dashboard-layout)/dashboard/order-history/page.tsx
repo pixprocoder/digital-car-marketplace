4bc27183a73ef136704eacf8bc4c22a76d98d8ad
samsul kobir <makkobir79@gmail.com>
2024-01-26 19:18:23 +0200

    bg changed

--- src/app/(dashboard-layout)/dashboard/order-history/page.tsx
+++ src/app/(dashboard-layout)/dashboard/order-history/page.tsx
@@ -15,15 +15,15 @@ const OrderHistoryPage = () => {
       <h1 className="text-white">OrderHistory</h1>
       <Table>
         <TableCaption>A list of your recent invoices.</TableCaption>
-        <TableHeader>
-          <TableRow>
+        <TableHeader className="">
+          <TableRow className="hover:bg-gray-900">
             <TableHead className="w-[100px]">Invoice</TableHead>
             <TableHead>Status</TableHead>
             <TableHead>Method</TableHead>
             <TableHead className="text-right">Amount</TableHead>
           </TableRow>
         </TableHeader>
-        <TableBody>
+        <TableBody className="bg-gray-500">
           <TableRow>
             <TableCell className="font-medium">INV001</TableCell>
             <TableCell>Paid</TableCell>
