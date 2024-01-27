046aceb9f33433d65ce0d14818c221c3ada7f803
samsul kobir <makkobir79@gmail.com>
2024-01-27 23:48:48 +0200

    user profile modified

--- src/app/(profile)/profile/page.tsx
+++ src/app/(profile)/profile/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
 const ProfilePage = () => {
+  const { data } = useSession();
   return (
     <div>
-      <h1 className="text-white text-2xl text-center"> Welcome Back</h1>
+      <h1 className="text-white text-2xl text-center">
+        {" "}
+        Welcome Back {data?.user?.name} 👋
+      </h1>
     </div>
   );
 };
--- src/components/pages/shared/ProfileSideBar.tsx
+++ src/components/pages/shared/ProfileSideBar.tsx
@@ -1,19 +1,23 @@
+"use client"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 
 const ProfileSideBarPage = () => {
+  const { data } = useSession();
+
   return (
-    <section className="min-h-[90vh] w-60  rounded-md bg-blue-900">
+    <section className="min-h-[90vh] w-60  rounded-md bg-gray-900">
       <div className="">
         <div className="text-white px-4 py-4 flex flex-col justify-center items-center">
           <Avatar className="w-12 h-12 rounded-full">
-            <AvatarImage />
+            <AvatarImage src={data?.user?.image} />
             <AvatarFallback />
           </Avatar>
-          <h1 className="text-2xl font-bold my-2">Smsul Kobir</h1>
-          <p className="text-sm">makkobir79@gmail.com</p>
+          <h1 className="text-2xl font-bold my-2">{data?.user?.name}</h1>
+          <p className="text-sm">{data?.user?.email}</p>
         </div>
         <hr />
       </div>
