0bd904a279171385caf555868fc6f3b762732ff4
samsul kobir <makkobir79@gmail.com>
2024-01-27 23:28:10 +0200

    dummy data replace and user data showd to dashboard sidebar

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AboutPage = () => {
   return (
-    <div className="container mx-auto">
+    <div className="min-h-screen container mx-auto">
       <h1 className="text-white">About Page</h1>
     </div>
   );
--- src/components/pages/shared/DashboardSideBar.tsx
+++ src/components/pages/shared/DashboardSideBar.tsx
@@ -1,19 +1,25 @@
+"use client";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useGetUsersQuery } from "@/redux/api/apiSlice";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 
 const DashboardSideBarPage = () => {
+  const { data } = useSession();
+  console.log(data?.user);
+
   return (
     <section className="min-h-[90vh] w-60  rounded-md bg-blue-900">
       <div className="">
         <div className="text-white px-4 py-4 flex flex-col justify-center items-center">
           <Avatar className="w-12 h-12 rounded-full">
-            <AvatarImage />
+            <AvatarImage src={data?.user?.image} />
             <AvatarFallback />
           </Avatar>
-          <h1 className="text-2xl font-bold my-2">Smsul Kobir</h1>
-          <p className="text-sm">makkobir79@gmail.com</p>
+          <h1 className="text-2xl font-bold my-2">{data?.user?.name}</h1>
+          <p className="text-sm">{data?.user?.email}</p>
         </div>
         <hr />
       </div>
