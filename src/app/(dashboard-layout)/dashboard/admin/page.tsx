70c6697f9cb6ba90a2db5523d374c9cbd58b0238
samsul kobir <makkobir79@gmail.com>
2024-01-29 21:38:11 +0200

    admin modified

--- src/app/(dashboard-layout)/dashboard/admin/page.tsx
+++ src/app/(dashboard-layout)/dashboard/admin/page.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const AdminPage = () => {
   return (
     <div>
-      <h1 className="text-white">AdminPage</h1>
+      <h1 className="text-white">
+        I am admin I can do what ever I want ha ha ha
+      </h1>
     </div>
   );
 };
