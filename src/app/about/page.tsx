347e892cfb6943cc48406017774a7b51e968c2b7
samsul kobir <makkobir79@gmail.com>
2024-02-04 23:54:39 +0200

    about us page modified

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,17 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const AboutPage = () => {
   return (
     <div className="min-h-screen container mx-auto">
       <h1 className="text-white">About Page</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+        corporis ipsa deleniti sint quasi cupiditate tenetur voluptas,
+        consequatur alias, quia, illo a corrupti ex vel quisquam minima at
+        eveniet. Neque?
+      </p>
+      <Button>Read More</Button>
     </div>
   );
 };
