60bb258e59d7a8ae85573a4758ed8bd0513d5ed8
samsul kobir <makkobir79@gmail.com>
2024-02-04 23:56:48 +0200

    super admin file modifired

--- src/app/(dashboard-layout)/dashboard/super_admin/page.tsx
+++ src/app/(dashboard-layout)/dashboard/super_admin/page.tsx
@@ -4,6 +4,13 @@ const SuperAdminPage = () => {
   return (
     <div>
       <h1 className="text-white">SuperAdmin</h1>
+      <h1>What super_admin can do</h1>
+      <ol>
+        <li>They can manage admin</li>
+        <li>They can create admin</li>
+        <li>They can change admin permission</li>
+        <li>They can manage user</li>
+      </ol>
     </div>
   );
 };
