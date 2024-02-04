edb1e54210e38bd6705769b705e541f9e23ea3d7
samsul kobir <makkobir79@gmail.com>
2024-02-04 23:57:57 +0200

    admin page modified

--- src/app/(dashboard-layout)/dashboard/admin/page.tsx
+++ src/app/(dashboard-layout)/dashboard/admin/page.tsx
@@ -6,6 +6,16 @@ const AdminPage = () => {
       <h1 className="text-white">
         I am admin I can do what ever I want ha ha ha
       </h1>
+
+      <div>
+        <h1>What can admin do</h1>
+        <ol>
+          <li>They can manage user</li>
+          <li>They can add user</li>
+          <li>They can remove user</li>
+          <li>They can block user</li>
+        </ol>
+      </div>
     </div>
   );
 };
